948a98de90798835cd22d9c12e7968064a81b316
ecmnet <ecm@gmx.de>
2017-11-24 07:44:35 +0100

    MAVLink update

---
+++ MAVComm/src/org/mavlink/messages/MAV_PARAM_EXT_TYPE.java
@@ -0,0 +1,55 @@
+/**
+ * Generated class : MAV_PARAM_EXT_TYPE
+ * DO NOT MODIFY!
+ **/
+package org.mavlink.messages;
+/**
+ * Interface MAV_PARAM_EXT_TYPE
+ * Specifies the datatype of a MAVLink extended parameter.
+ **/
+public interface MAV_PARAM_EXT_TYPE {
+    /**
+     * 8-bit unsigned integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_UINT8 = 1;
+    /**
+     * 8-bit signed integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_INT8 = 2;
+    /**
+     * 16-bit unsigned integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_UINT16 = 3;
+    /**
+     * 16-bit signed integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_INT16 = 4;
+    /**
+     * 32-bit unsigned integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_UINT32 = 5;
+    /**
+     * 32-bit signed integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_INT32 = 6;
+    /**
+     * 64-bit unsigned integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_UINT64 = 7;
+    /**
+     * 64-bit signed integer
+     */
+    public final static int MAV_PARAM_EXT_TYPE_INT64 = 8;
+    /**
+     * 32-bit floating-point
+     */
+    public final static int MAV_PARAM_EXT_TYPE_REAL32 = 9;
+    /**
+     * 64-bit floating-point
+     */
+    public final static int MAV_PARAM_EXT_TYPE_REAL64 = 10;
+    /**
+     * Custom Type
+     */
+    public final static int MAV_PARAM_EXT_TYPE_CUSTOM = 11;
+}
