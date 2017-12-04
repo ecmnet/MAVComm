e981f65ea8d9d315bdf0134fbac617ef747165ad
ecmnet <ecm@gmx.de>
2017-12-04 19:33:43 +0100

    CleanUp

--- MAVComm/src/com/comino/msp/slam/map/ILocalMap.java
+++ MAVComm/src/com/comino/msp/slam/map/ILocalMap.java
@@ -38,7 +38,6 @@ import com.comino.msp.model.DataModel;
 import boofcv.struct.image.GrayU8;
 import georegression.struct.point.Point3D_F64;
 import georegression.struct.point.Vector3D_F32;
-import georegression.struct.point.Vector3D_F64;
 import georegression.struct.point.Vector4D_F64;
 
 public interface ILocalMap {
