2e03673c6f21b3e100a796e971ed9d904ddc8694
ecmnet <ecm@gmx.de>
2018-02-08 07:40:39 +0100

    Indent fix

--- MAVComm/src/com/comino/mav/mavlink/MAV_CUST_MODE.java
+++ MAVComm/src/com/comino/mav/mavlink/MAV_CUST_MODE.java
@@ -39,20 +39,20 @@ public class MAV_CUST_MODE {
 	public static int PX4_CUSTOM_MAIN_MODE_MANUAL 		= 1;
 	public static int PX4_CUSTOM_MAIN_MODE_ALTCTL 		= 2;
 	public static int PX4_CUSTOM_MAIN_MODE_POSCTL 		= 3;
-	public static int PX4_CUSTOM_MAIN_MODE_AUTO 		= 4;
-	public static int PX4_CUSTOM_MAIN_MODE_ACRO 		= 5;
-	public static int PX4_CUSTOM_MAIN_MODE_OFFBOARD 	= 6;
+	public static int PX4_CUSTOM_MAIN_MODE_AUTO 		    = 4;
+	public static int PX4_CUSTOM_MAIN_MODE_ACRO 		    = 5;
+	public static int PX4_CUSTOM_MAIN_MODE_OFFBOARD 	    = 6;
 	public static int PX4_CUSTOM_MAIN_MODE_STABILIZED 	= 7;
-	public static int PX4_CUSTOM_MAIN_MODE_RATTITUDE    = 8;
+	public static int PX4_CUSTOM_MAIN_MODE_RATTITUDE     = 8;
 
-	public static int PX4_CUSTOM_SUB_MODE_AUTO_READY    = 1;
+	public static int PX4_CUSTOM_SUB_MODE_AUTO_READY     = 1;
 	public static int PX4_CUSTOM_SUB_MODE_AUTO_TAKEOFF 	= 2;
 	public static int PX4_CUSTOM_SUB_MODE_AUTO_LOITER 	= 3;
 	public static int PX4_CUSTOM_SUB_MODE_AUTO_MISSION 	= 4;
 	public static int PX4_CUSTOM_SUB_MODE_AUTO_RTL 		= 5;
-	public static int PX4_CUSTOM_SUB_MODE_AUTO_LAND 	= 6;
-	public static int PX4_CUSTOM_SUB_MODE_AUTO_RTGS 	= 7;
-	public static int PX4_CUSTOM_SUB_MODE_AUTO_FOLLOW_ME= 8;
+	public static int PX4_CUSTOM_SUB_MODE_AUTO_LAND 		= 6;
+	public static int PX4_CUSTOM_SUB_MODE_AUTO_RTGS 		= 7;
+	public static int PX4_CUSTOM_SUB_MODE_AUTO_FOLLOW_ME	= 8;
 
 	private static String[] mode_string = {
 			"Manual","Acro","Stabilized","Rattitude","Altitude Control","Position Control",
