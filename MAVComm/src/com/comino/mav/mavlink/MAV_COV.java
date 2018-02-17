16bc9a69b656c34bd6bcdd34416fe2e292a9226c
ecmnet <ecm@gmx.de>
2018-02-17 07:05:41 +0100

    COV Indexes fo msg_local_position_ned_cov added

---
+++ MAVComm/src/com/comino/mav/mavlink/MAV_COV.java
@@ -0,0 +1,16 @@
+package com.comino.mav.mavlink;
+
+public class MAV_COV {
+
+	// COV indexes for msg_local_position_ned_cov
+
+	public final static int VIS_COV_X 		= 0;
+	public final static int VIS_COV_Y 		= 9;
+	public final static int VIS_COV_Z 		= 17;
+
+	public final static int VIS_COV_VX 		= 24;
+	public final static int VIS_COV_VY 		= 30;
+	public final static int VIS_COV_VZ 		= 35;
+
+
+}
