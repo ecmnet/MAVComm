43388c79430b40c5e0f2af827a9a04fb4c3f741f
ecmnet <ecm@gmx.de>
2018-07-03 22:19:11 +0200

    Autopilot: Fixing land command

--- MAVComm/src/com/comino/msp/execution/autopilot/Autopilot2D.java
+++ MAVComm/src/com/comino/msp/execution/autopilot/Autopilot2D.java
@@ -313,7 +313,7 @@ public class Autopilot2D implements Runnable {
 			logger.writeLocalMsg("[msp] Autopilot: Home reached.",MAV_SEVERITY.MAV_SEVERITY_INFO);
 			ExecutorService.get().schedule(()-> {
 				model.sys.setAutopilotMode(MSP_AUTOCONTROL_ACTION.RTL, false);
-				control.sendMAVLinkCmd(MAV_CMD.MAV_CMD_NAV_LAND, 0, 2, 0.05f );
+				control.sendMAVLinkCmd(MAV_CMD.MAV_CMD_NAV_LAND, 5, 0, 0, 0.05f );
 			}, delay_ms, TimeUnit.MILLISECONDS);
 			control.sendMAVLinkMessage(new msg_msp_micro_slam(2,1));
 		});
