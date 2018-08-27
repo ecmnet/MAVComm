9e06db1ec95d9ab00f38926f067f6f97bf91c7f2
ecmnet <ecm@gmx.de>
2018-08-27 14:01:51 +0200

    DenoiseMap Filter

--- MAVComm/src/com/comino/msp/slam/map2D/filter/impl/DenoiseMapFilter.java
+++ MAVComm/src/com/comino/msp/slam/map2D/filter/impl/DenoiseMapFilter.java
@@ -14,13 +14,13 @@ public class DenoiseMapFilter implements ILocalMapFilter {
 
 	private final GrayF32 map_in;
 	private final GrayF32 map_out;
-	private final BlurFilter<GrayF32> blurrer;
+	private final WaveletDenoiseFilter<GrayF32> blurrer;
 
 
 	public DenoiseMapFilter(int width, int height) {
 		this.map_in = new GrayF32(width,height);
 		this.map_out = new GrayF32(width,height);
-		this.blurrer = FactoryBlurFilter.gaussian(GrayF32.class,2.5,2);
+		this.blurrer = FactoryImageDenoise.waveletBayes(GrayF32.class,4,70,20000);
 	}
 
 	@Override
