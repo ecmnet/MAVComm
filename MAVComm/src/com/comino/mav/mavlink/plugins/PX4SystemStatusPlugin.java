62642699233aa58f66bbdfcf58ffdaacd4e4ed09
ecmnet <ecm@gmx.de>
2019-07-22 16:26:50 +0200

    Separate Battery info from system status (WIP)

--- MAVComm/src/com/comino/mav/mavlink/plugins/PX4SystemStatusPlugin.java
+++ MAVComm/src/com/comino/mav/mavlink/plugins/PX4SystemStatusPlugin.java
@@ -12,14 +12,16 @@ public class PX4SystemStatusPlugin extends MAVLinkPluginBase {
 	public void received(Object o) {
 
 		msg_sys_status sys = (msg_sys_status) o;
-		model.battery.p = (short) sys.battery_remaining;
-		model.battery.b0 = sys.voltage_battery / 1000f;
-		model.battery.c0 = sys.current_battery / 100f;
-		model.battery.tms = System.currentTimeMillis() * 1000;
+		if(sys.battery_remaining != -1) {
+			model.battery.p = (short) sys.battery_remaining;
+			model.battery.b0 = sys.voltage_battery / 1000f;
+			model.battery.c0 = sys.current_battery / 100f;
+			model.battery.tms = System.currentTimeMillis() * 1000;
+		}
 
 		model.sys.error1 = sys.errors_count1;
 		if(sys.load > 0)
-		  model.sys.load_p = sys.load / 10;
+			model.sys.load_p = sys.load / 10;
 		model.sys.drops_p = sys.drop_rate_comm / 10000f;
 
 
